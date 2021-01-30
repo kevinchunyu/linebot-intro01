a177d7288b1ef6a0148373fca36dab367d9bf82a
kevinchunyu <>
2021-01-31 01:17:16 +0800

    updated code and file to not include express

--- app/app.js
+++ app/app.js
@@ -1,28 +1,17 @@
-const express = require('express');
+let linebot = require('linebot');
 
-const app = express();
-const linebot = require('linebot');// 判別開發環境
-
-if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
- require('dotenv').config()                      // 使用 dotenv 讀取 .env 檔案
-};
-
-const bot = linebot({
- channelId: process.env.CHANNEL_ID,
- channelSecret: process.env.CHANNEL_SECRET,
- channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+let bot = linebot({
+  channelId: '1655623292',
+  channelSecret: '878ea3b73da5839afdf76fc88e68031b',
+  channelAccessToken: 'tpet8lw5YNWo94OACtnY4b5Wro6rKjghAXXROKIY71tBHau9XMwjmxxQn+kr+fDkt9F3YTS8i8mNsqZwFK4EJkhmj6263yuYrZQOgbYeLqUZ1TwN/xErdHRNYSWpVf+ohUjLsSNgocpyxXQNICW4qAdB04t89/1O/w1cDnyilFU='
 });
 
-const linebotParser = bot.parser();bot.on('message', function (event) {
- console.log(event);
- event.reply(event.message.text).then(function (data) {
-   // success
- }).catch(function (error) {
-   // error
- });
+
+bot.on('message', function (event) {
+  // reply bot
+  event.reply(`you said ${event.message.text}`);
 });
 
-app.post('/', linebotParser);
-app.listen(process.env.PORT || 3000, () => {
- console.log('Express server start')
+bot.listen('/', process.env.PORT || 5000, function () {
+  console.log('Bot has successfully been deployed!');
 });
