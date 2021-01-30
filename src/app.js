8d18df089a7a26d1de60e4c16e2bf78f21f83ac9
kevinchunyu <>
2021-01-31 03:06:50 +0800

    updated npm installs

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-let linebot = require('linebot');
+const express = require('express')
+const app = express()
+const linebot = require('linebot');
 
-let bot = linebot({
+if (process.env.NODE_ENV !== 'production') {
+ require('dotenv').config()
+}
+
+const bot = linebot({
   channelId: '1655623292',
   channelSecret: '878ea3b73da5839afdf76fc88e68031b',
   channelAccessToken: 'tpet8lw5YNWo94OACtnY4b5Wro6rKjghAXXROKIY71tBHau9XMwjmxxQn+kr+fDkt9F3YTS8i8mNsqZwFK4EJkhmj6263yuYrZQOgbYeLqUZ1TwN/xErdHRNYSWpVf+ohUjLsSNgocpyxXQNICW4qAdB04t89/1O/w1cDnyilFU='
 });
 
+const linebotParser = bot.parser();
 
 bot.on('message', function (event) {
-  // reply bot
-  event.reply(`you said ${event.message.text}`);
+ console.log(event);event.reply(event.message.text).then(function (data) {
+   // success
+ }).catch(function (error) {
+   // error
+   console.log("Error is", error);
+ });
 });
 
-bot.listen('/', process.env.PORT || 5000, function () {
-  console.log('Bot has successfully been deployed!');
+app.post('/', linebotParser);
+app.listen(process.env.PORT || 8000, () => {
+ console.log('Express server start')
 });
